a703c337359e629ea294d55e350cc51caf3b87f2
knyttali <amanda_andrae@hotmail.com>
2021-11-18 14:37:31 +0100

    lala

--- app/src/main/java/net/miwashi/stijdbc/Artist.java
+++ app/src/main/java/net/miwashi/stijdbc/Artist.java
@@ -1,8 +1,32 @@
 package net.miwashi.stijdbc;
 
+import java.util.Set;
+
 public class Artist {
+    private Integer id;
+    private String name;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
 
     public Artist(Integer id, String name) {
+    this.id = id;
+    this.name = name;
+    }
+
+    public String toString(){
+        return id + " " + name;
     }
 
 }
